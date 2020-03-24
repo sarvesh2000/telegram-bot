a0e49e15ef64b676a3c684e09f70c83f848a1382
sarvesh2000 <sarvesh4232@gmail.com>
2020-03-24 23:37:59 +0530

    Heroku Link in config.py

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
 TOKEN = '1094258301:AAFtCHuBwqp4yN5GdFYl8YFqZbSWPwTyyrg'
-NGROK_URL = 'http://ec2-100-25-216-78.compute-1.amazonaws.com'
+NGROK_URL = 'https://tvs-telegram-bot.herokuapp.com'
 BASE_TELEGRAM_URL = 'https://api.telegram.org/bot{}'.format(TOKEN)
 LOCAL_WEBHOOK_ENDPOINT = '{}/webhook'.format(NGROK_URL)
 TELEGRAM_INIT_WEBHOOK_URL = '{}/setWebhook?url={}'.format(BASE_TELEGRAM_URL, LOCAL_WEBHOOK_ENDPOINT)
